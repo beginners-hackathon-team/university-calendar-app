0421b330581359de8ac3f1afccb168fd98de00e0
nogi <kazuto1670@gmail.com>
2026-04-18 17:56:32 +0900

    時間割登録画面

--- frontend/src/pages/CoursesPage.tsx
+++ frontend/src/pages/CoursesPage.tsx
@@ -1,3 +1,24 @@
 export default function CoursesPage() {
-    return (<h1>時間割</h1>)
+  const days = [" ", "月", "火", "水", "木", "金"];
+  const periods = [1, 2, 3, 4, 5, 6];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+            {days.map((d) => <th key={d}>{d}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {periods.map((p) => (
+            <tr key={p}>
+            <th>{p}限</th>
+            {days.map((d) => <td key={d}>d</td>)}
+            {/* ★ ここで days.map して <td key=...></td> を7個 */}
+            </tr>
+        ))}
+        </tbody>
+
+    </table>
+  );
 }
