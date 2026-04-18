88a528aa1850f95bbf394d9cc57bc22e3592f075
takuma2004 <w2252100299@stu.kanazawa-u.ac.jp>
2026-04-18 16:14:28 +0900

    aaa

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // SPAのためのルーティング
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import APP from "./APP";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ <StrictMode>
     <BrowserRouter>
-      <App />
+       <APP />
     </BrowserRouter>
-  </StrictMode>
+ </StrictMode>
 )
