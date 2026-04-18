428e0d048a73a900d723bba2b4b98cfd316d0fe1
渡部 <wyuu1006@gmail.com>
2026-04-18 16:05:36 +0900

    get_user, get_users, delete追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
 class CreateUser(BaseModel):
     name: str
+    pas: str
+    mail: EmailStr
 
 
 class ReadUser(BaseModel):
@@ -15,3 +17,36 @@ class ReadUser(BaseModel):
 @app.get("/api/health")
 def health():
     return {"status": "ok"}
+
+
+list = []
+
+
+@app.post("/spi/user")
+def create_user(user: CreateUser):
+    list.append(user.name)
+    return user
+
+
+@app.get("/api/user")
+def get_users():
+    return list
+
+
+@app.get("/api/user/{user_name}")
+def get_user(user_name: str):
+    if user_name in list:
+        return user_name
+
+    else:
+        return -1
+
+
+@app.delete("/api/user")
+def delete_user(user_name: str):
+    if user_name in list:
+        list.remove(user_name)
+        return list
+
+    else:
+        return -1
