66afa39b4da5a2428c0f8bd13379cfd2b9d6aa7c
nogi <kazuto1670@gmail.com>
2026-04-18 16:20:09 +0900

    main.tsx修正

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import APP from "./APP";
+import App from "./App";
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
     <BrowserRouter>
-       <APP />
+       <App />
     </BrowserRouter>
  </StrictMode>
 )
