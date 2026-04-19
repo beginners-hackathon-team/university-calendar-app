3a6da4cdda4870a58d0e64a4e4f95feef2e2511c
okajima ryota <okajima-r@okajimanoMacBook-Air.local>
2026-04-19 13:29:44 +0900

    coursemodelにteacher追加

--- backend/app/models/course.py
+++ backend/app/models/course.py
@@ -10,3 +10,4 @@ class Course(Base):
     id: Mapped[str] = mapped_column(String, primary_key=True, default=uuid_str)
     name: Mapped[str] = mapped_column(String)
     room: Mapped[str] = mapped_column(String)
+    teacher: Mapped[str] = mapped_column(String)
